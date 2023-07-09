a6b78f562718af52da20eb0ee786f7b52474f591
Kamila Adamska <121kama121@gmail.com>
2023-07-09 21:15:24 +0200

    Refactored Home and App components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,15 @@
-import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 import { SharedLayout } from './SharedLayout';
-import { fetchTrendingMovies } from 'services/fetchMovies';
 
 const Home = lazy(() => import('../pages/Home/Home'));
 const Movies = lazy(() => import('../pages/Movies/Movies'));
 
 export const App = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const getMovies = async () => {
-      try {
-        const newData = await fetchTrendingMovies();
-        const newMovies = newData.results;
-        setMovies(newMovies);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-
-    getMovies();
-  }, []);
-
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home movies={movies} />} />
+        <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
       </Route>
     </Routes>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,24 @@
+import { useState, useEffect } from 'react';
+import { fetchTrendingMovies } from 'services/fetchMovies';
 import { Headline, ListItem, List } from './Home.styled';
 
-const Home = ({ movies }) => {
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const newData = await fetchTrendingMovies();
+        const newMovies = newData.results;
+        setMovies(newMovies);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    getMovies();
+  }, []);
+
   return (
     <>
       <Headline>Trending today</Headline>
